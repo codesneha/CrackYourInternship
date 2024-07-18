21859eda0a9f8dcfaeaef5504c4544c9f386a668
codesneha <sneha.jain@cumminscollege.in>
2024-07-18 13:18:20 +0530

    Added solution for day 1

---
+++ sol6.java
@@ -0,0 +1,13 @@
+/*Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array */
+
+import java.util.Arrays;
+
+class sol6 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        return nums[n/2];
+    }
+}
