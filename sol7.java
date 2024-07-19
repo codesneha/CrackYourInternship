77c5242ebc76e57fb2cc31847dc300e848b5c893
codesneha <sneha.jain@cumminscollege.in>
2024-07-19 16:56:28 +0530

    Added solution for day 2

---
+++ sol7.java
@@ -0,0 +1,42 @@
+/*Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node. */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ * @param <ListNode>
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null; // Handle empty list case
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow; // Return the middle node
+    }
+}
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
