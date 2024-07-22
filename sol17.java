ca7a432f58ded591fa85e9f26933201d16241c81
codesneha <sneha.jain@cumminscollege.in>
2024-07-22 21:55:36 +0530

    Added solution for day 4

---
+++ sol17.java
@@ -0,0 +1,26 @@
+/*Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.*/
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class sol17 {
+    ListNode curr;
+    public boolean isPalindrome(ListNode head) {
+        curr=head;
+        return solve(head);
+    }
+    public boolean solve(ListNode head){
+        if(head == null) return true;
+        boolean ans=solve(head.next) && head.val ==curr.val;
+        curr=curr.next;
+        return ans;
+    }
+}
